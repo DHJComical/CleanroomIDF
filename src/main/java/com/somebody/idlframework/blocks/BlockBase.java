da91b048ebfa69693af026d3c3b1a4226a4cf4a3
Comical <dhjcomical@163.com>
2025-05-11 10:42:06 +0800

    Change IDF to Cleanroom TemplateDevEnv

---
+++ src/main/java/com/somebody/idlframework/blocks/BlockBase.java
@@ -0,0 +1,49 @@
+package com.somebody.idlframework.blocks;
+
+import java.util.Random;
+
+import com.somebody.idlframework.IdlFramework;
+import com.somebody.idlframework.init.ModCreativeTab;
+import com.somebody.idlframework.item.ModItems;
+import com.somebody.idlframework.util.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class BlockBase extends Block implements IHasModel
+{
+	public BlockBase(String name, Material material)
+	{
+		super(material);
+		setRegistryName(name);
+		setRegistryName(name);
+		setCreativeTab(ModCreativeTab.IDL_MISC);;
+		
+		ModBlocks.BLOCKS.add(this);
+		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+
+		setHardness(5.0F);
+		//setResistance(1000.0F);
+		//setHarvestLevel("pickaxe", 1);
+		//setLightLevel(1f);
+		setLightOpacity(1);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return super.getItemDropped(state, rand, fortune);
+	}
+
+	@Override
+	public int quantityDropped(Random rand) {
+		return super.quantityDropped(rand);
+	}
+	
+	@Override
+	public void registerModels() {
+		IdlFramework.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
